4e9bbc8ab914c3fb2931701d7c920cf8c33ce142
Tom Waddington <tom@implementor.org>
2019-12-01 19:27:35 +0000

    vec->set for constant time membership checking, much faster

--- 2018/1/src/one/core.clj
+++ 2018/1/src/one/core.clj
@@ -6,31 +6,31 @@
   [input]
   (apply + input))
 
-(defn- found-first-number?
-  [possibility already]
-  (and (number? possibility)
-       (nil? already)))
-
 (defn first-duplicate-frequency-reached
   "Return the first frequency reached twice by applying frequency changes repeatedly."
   [input]
-  (let [duplicate (atom nil)]
-    (loop [journal [0]]
-      (if (nil? @duplicate)
-        (recur (loop [next-frequency        (first input)
-                      remaining-frequencies (rest input)
-                      journal               journal]
-                 (let [frequency (+ (last journal) next-frequency)]
-                   (when (found-first-number? (some #{frequency} journal)
-                                              @duplicate)
-                     (reset! duplicate frequency))
-                   (if (and (seq remaining-frequencies)
-                            (nil? @duplicate))
-                     (recur (first remaining-frequencies)
-                            (rest remaining-frequencies)
-                            (conj journal frequency))
-                     (conj journal frequency)))))
-        @duplicate))))
+  (loop [cycle {:journal   #{0}
+                :frequency 0
+                :duplicate nil}]
+    (if (nil? (:duplicate cycle))
+      (recur (loop [next-frequency        (first input)
+                    remaining-frequencies (rest input)
+                    journal               (:journal cycle)
+                    current-frequency     (:frequency cycle)]
+               (let [frequency (+ current-frequency next-frequency)
+                     duplicate (if (contains? journal frequency)
+                                 frequency
+                                 nil)]
+                 (if (and (seq remaining-frequencies)
+                          (nil? duplicate))
+                   (recur (first remaining-frequencies)
+                          (rest remaining-frequencies)
+                          (conj journal frequency)
+                          frequency)
+                   {:journal   (conj journal frequency)
+                    :frequency frequency
+                    :duplicate duplicate}))))
+      (:duplicate cycle))))
 
 (defn -main
   [& args]
