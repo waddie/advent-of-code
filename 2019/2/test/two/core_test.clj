b88adf90282ed0c735c7b7a9cd40570d197c6eaf
Tom Waddington <tom@implementor.org>
2019-12-02 15:03:45 +0000

    2019 day two

--- 2019/2/test/two/core_test.clj
+++ 2019/2/test/two/core_test.clj
@@ -5,3 +5,12 @@
 (deftest example-for-part-one
   (testing "Example for part one."
     (is (= 3500 (two.core/process-opcodes [1 9 10 3 2 3 11 0 99 30 40 50])))))
+
+(deftest example-for-part-two
+  (testing "Check part two finds original inputs for part one."
+    (let [input (mapv read-string (clojure.string/split
+                                   (slurp "resources/input.txt")
+                                   #","))
+          {noun :noun
+           verb :verb} (two.core/search-for-inputs input 4138687)]
+      (is (= 1202 (+ (* 100 noun) verb))))))
