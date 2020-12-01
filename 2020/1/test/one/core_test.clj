98ccfcd11a5aaa8bdcc473abcccf81655f250e85
Tom Waddington <tom@implementor.org>
2020-12-01 08:30:43 +0000

    2020/1

--- 2020/1/test/one/core_test.clj
+++ 2020/1/test/one/core_test.clj
@@ -1,18 +1,19 @@
 (ns one.core-test
   (:require [clojure.test :refer :all]
-            [clojure.string :as string]
             [one.core :refer :all]))
 
 (def example-array [1721 979 366 299 675 1456])
+(def example-one-answer 514579)
+(def example-two-answer 241861950)
 
 (deftest example-for-part-one
   (testing "Example for part one."
-    (is (= 514579
+    (is (= example-one-answer
            (reduce * (one.core/find-pair-equal-to-sum example-array
                                                       one.core/target-number))))))
 
 (deftest example-for-part-two
   (testing "Example for part two."
-    (is (= 241861950
+    (is (= example-two-answer
            (reduce * (one.core/find-triplet-equal-to-sum example-array
                                                          one.core/target-number))))))
